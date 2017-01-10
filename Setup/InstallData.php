435a928a0d612be69ebc1e3a6776dfc178c82927
Rahmane OUSMANE <ousmanedev@gmail.com>
2017-01-10 10:28:15 +0800

    Improve adding ‘botamp_entity_id’ attribute script

--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -8,41 +8,39 @@ use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 
-class InstallData implements InstallDataInterface
-{
+class InstallData implements InstallDataInterface {
     private $eavSetupFactory;
 
-    public function __construct(EavSetupFactory $eavSetupFactory)
-    {
+    public function __construct(EavSetupFactory $eavSetupFactory) {
       $this->eavSetupFactory = $eavSetupFactory;
     }
 
-    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
-    {
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context) {
       $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
       $eavSetup->addAttribute(
         \Magento\Catalog\Model\Product::ENTITY,
         'botamp_entity_id',
         [
-            'type' => 'int',
-            'backend' => '',
-            'frontend' => '',
-            'label' => 'Botamp Entity ID',
-            'input' => '',
-            'class' => '',
-            'source' => '',
-            'global' => \Magento\Catalog\Model\Resource\Eav\Attribute::SCOPE_GLOBAL,
-            'visible' => true,
-            'required' => false,
-            'user_defined' => false,
-            'default' => 0,
-            'searchable' => false,
-            'filterable' => false,
-            'comparable' => false,
-            'visible_on_front' => false,
-            'used_in_product_listing' => true,
-            'unique' => true,
-            'apply_to' => ''
+          'type' => 'int',
+          'backend' => '',
+          'frontend' => '',
+          'label' => 'Botamp Entity ID',
+          'input' => '',
+          'class' => '',
+          'source' => '',
+          'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+          'visible' => false,
+          'required' => false,
+          'user_defined' => false,
+          'default' => 0,
+          'searchable' => false,
+          'filterable' => false,
+          'comparable' => false,
+          'visible_on_front' => false,
+          'used_in_product_listing' => false,
+          'unique' => true,
+          'apply_to' => ''
         ]
       );
     }
